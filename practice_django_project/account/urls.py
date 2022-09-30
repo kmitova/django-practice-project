9607f46520244195b3c236dd1d86e0141a28f357
kmitova <96522051+kmitova@users.noreply.github.com>
2022-09-30 16:39:01 +0300

    added login and register pages (views, urls, html, css)

--- practice_django_project/account/urls.py
+++ practice_django_project/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from practice_django_project.account.views import login_user
+from practice_django_project.account.views import login_user, register_user
 
 urlpatterns = (
     path('login/', login_user, name='login user'),
+    path('register/', register_user, name='register user'),
 )
