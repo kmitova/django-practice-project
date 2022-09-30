f428dfc4333e6614ed155b6ab34dbd559d7b9c3c
kmitova <96522051+kmitova@users.noreply.github.com>
2022-09-30 21:36:32 +0300

    added profile details pages (views, urls, html, css)

--- practice_django_project/account/urls.py
+++ practice_django_project/account/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
-from practice_django_project.account.views import login_user, register_user
+from practice_django_project.account.views import login_user, register_user, details_user
 
 urlpatterns = (
     path('login/', login_user, name='login user'),
     path('register/', register_user, name='register user'),
+    path('profile/<int:pk>/', include([
+        path('', details_user, name='details user'),
+        ]))
 )
