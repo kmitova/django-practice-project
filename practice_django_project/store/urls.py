de82c288b5f266253776a02b6d70b456acd1a269
kmitova <96522051+kmitova@users.noreply.github.com>
2022-10-03 16:41:51 +0300

    work on store app

--- practice_django_project/store/urls.py
+++ practice_django_project/store/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
-from practice_django_project.store.views import store_main_page
+from practice_django_project.store.views import store_main_page, details_book
 
 urlpatterns = (
     path('', store_main_page, name='store main page'),
+    path('book/<int:pk>/', include([
+        path('', details_book, name='details book'),
+        ]))
 )
+
