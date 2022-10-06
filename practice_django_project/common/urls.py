5896038a3532627847c63e6aadac192862a8ad4f
kmitova <96522051+kmitova@users.noreply.github.com>
2022-10-06 15:05:18 +0300

    work on footer pages (urls, views, html); home link

--- practice_django_project/common/urls.py
+++ practice_django_project/common/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from practice_django_project.common.views import index
+from practice_django_project.common.views import index, about, team, t_and_c, delivery, faq
 
 urlpatterns = (
     path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('team/', team, name='team'),
+    path('t-and-c/', t_and_c, name='t-and-c'),
+    path('delivery/', delivery, name='delivery'),
+    path('faq/', faq, name='faq'),
 )
--- practice_django_project/common/views.py
+++ practice_django_project/common/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'common/index.html')
+
+
+def about(request):
+    return render(request, 'common/about.html')
+
+
+def team(request):
+    return render(request, 'common/team.html')
+
+
+def t_and_c(request):
+    return render(request, 'common/t-and-c.html')
+
+
+def delivery(request):
+    return render(request, 'common/delivery.html')
+
+
+def faq(request):
+    return render(request, 'common/faq.html')
