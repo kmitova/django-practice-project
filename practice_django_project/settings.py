18e563a7c936d2b75e88ee5a97b38ee72d449bcb
kmitova <96522051+kmitova@users.noreply.github.com>
2022-10-07 14:20:02 +0300

    database work setup

--- practice_django_project/settings.py
+++ practice_django_project/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'practice_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
