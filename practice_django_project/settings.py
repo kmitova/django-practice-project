b49dffd183f41943e2f5764a6d716eab139ef425
kmitova <96522051+kmitova@users.noreply.github.com>
2022-10-18 10:36:50 +0300

    configurations for media files support

--- practice_django_project/settings.py
+++ practice_django_project/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media_files'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
